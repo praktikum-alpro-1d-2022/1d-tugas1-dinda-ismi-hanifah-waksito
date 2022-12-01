768fe887d7cf81b452d77c2c22b14bbf0ec9fe00
Dihw26 <115917450+Dihw26@users.noreply.github.com>
2022-12-01 23:08:42 +0700

    Add files via upload

---
+++ TUGAS 1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+    string nama,kelas,hobi,cita,Npm,umur;
+
+
+
+    cout<<"Ini Program  untuk Menyimpan Biodata " <<endl;
+    cout<<"====================================="<<endl;
+    cout<<"Masukan data Berikut "<<endl;
+    cout<<"Masukkan Npm 2210631170116: " ; cin>> Npm;
+    cout<<"Massukan Nama Dinda Ismi Hanifah Waskito: "; cin >>nama;
+    cout<<"Masukan Kelas 1D-informatika: "; cin>>kelas;
+    cout<<"Masukkan Umur 18 tahun: "; cin>> umur;
+    cout<<"Masukan Hobi Main game, memasak: "; cin >> hobi;
+    cout<<"Cita cita di masa depan Bisa membanggakan untuk orang tua: "; cin>> cita;
+    cout<<"===================================="<<endl;
+    cout<<"Ini adalah Biodata Saya" <<endl;
+    cout<<"================================"<<endl;
+
+    cout<<"Nama Saya adalah Dinda Ismi Hanifah Waskito :"<<nama; cout<<endl;
+    cout<<"Npm Saya adalah 2210631170116:"<<Npm; cout<<endl;
+    cout<<"Kelas Saya di 1D-Informatika: "<<kelas ;cout<<endl;
+    cout<<"Umur Saya Sekarang 18 tahun: "<<umur; cout<<endl;
+    cout<<"Hobi Saya Main game, memasak: "<<hobi; cout<<endl;
+    cout<<"Cita Cita Saya Bisa membanggakan orang tua: "<<cita; cout<<endl;
+
+
+
+
+
+}
